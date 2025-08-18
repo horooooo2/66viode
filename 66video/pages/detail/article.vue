694556543d7608dfe477ab09fc1e478eafe32a5d
horo1111 <qrerqwe3@gmail.com>
2025-08-18 15:16:04 +0800

    update

--- 66video/pages/detail/article.vue
+++ 66video/pages/detail/article.vue
@@ -1,61 +1,149 @@
 <template>
 	<view class="my_detail">
-        <view class="status_bar"></view>
-		
-        <view class="d_title"> 
-            <text>详情666666666顺丰收到是是是是是顺丰收到是是是是是顺丰收到是是是是是顺丰收到是是是是是顺丰收到是是是是是顺丰收到</text>  
-        </view>
-		<view class="d_time"><text>吃瓜视频 </text>发布于<text>2025-05-15 {{store?.user?.userData?.name}}</text></view>
-		<view class="d_cont">
-			<rich-text :nodes="strings"></rich-text>
+		<NavBar title="吃瓜"></NavBar>
+
+		<view class="eat-container">
+			<view class="d_title">{{ dataItem.title }}</view>
+			<view class="d_time"><text>吃瓜视频 </text>发布于<text>{{ dataItem.created_at }} {{store?.user?.userData?.name}}</text></view>
+			<view class="d_cont" v-for="(item, index) in dataItem.content" :key="index">
+				<rich-text v-if="item.type === 'richtext'" :nodes="item.content"></rich-text>
+				
+				<view class="video-box" v-if="item.type === 'video'">
+					<videoDom :detailData="{ data: { type: item.type, content: item.content.url, mobile_image: dataItem.mobile_image } }" />
+					<view class="video-label">
+						<view class="video-label-item">
+							<view class="text" v-for="val in dataItem.hash_tags" :key="val">#{{ val }}</view>
+						</view>
+					</view>
+				</view>
+			</view>
+
+			<Like :detailData="{ data: { like_count: dataItem.like_count, view_count: dataItem.view_count, comment_count: dataItem.comment_count } }" />
+			<Share />
+			<Sponsor />
+			<Critique />
 		</view>
-		
-		<Like />
-		<Share />
-		<Sponsor />
-		<Critique />
 	</view>
-	
 </template>
 
 <script setup>
-import { ref, defineEmits, defineProps, watchEffect,onMounted } from 'vue'
-import Like from './components/like.vue'
-import Share from './components/share.vue'
-import Sponsor from './components/sponsor.vue'
-import Critique from './components/critique.vue'
-const strings = ref('<div><p>富文本数据......</p></div>');
+	import {
+		ref,
+		defineEmits,
+		defineProps,
+		watchEffect,
+		onMounted,
+	} from 'vue'
+	import {
+		useRoute
+	} from 'vue-router'
+	import NavBar from '@/components/NavBar/index.vue'
+	import videoDom from '../video/index.vue'
+	import Like from './components/like.vue'
+	import Share from './components/share.vue'
+	import Sponsor from './components/sponsor.vue'
+	import Critique from './components/critique.vue'
+	import {
+		apiGetArticleDetail,
+	} from '@/common/api/content.js'
 
+	const route = useRoute()
 
-onMounted(() => {
+	const strings = ref('<div><p>富文本数据......</p></div>');
+	const dataItem = ref('')
 
-})
+	onMounted(() => {
+		getData()
+	})
+	
+	const getData = () => {
+		apiGetArticleDetail({
+			id: route.query.id
+		}).then(res => {
+			if (res.code === 0) {
+				dataItem.value = res.data
+			} else {
+				uni.showToast({
+					title: res.msg,
+					icon: 'none',
+					duration: 2000
+				});
+			}
+		})
+	}
 
-const setUser = ()=>{
-	store.user.setUser({name:9999});
-}
-
-
+	const setUser = () => {
+		store.user.setUser({
+			name: 9999
+		});
+	}
 </script>
 <style lang="scss" scoped>
-.my_detail{
-	padding: 20rpx;
-    background: #111;
-	.d_title{
-		color: #fff;
-		font-size: 28rpx;
-	}
-	.d_time{
-		margin-top: 18rpx;
-		color: #aaa;
-		font-size: 23rpx;
-	}
-	.d_cont{
-		padding: 20rpx 0;
-		color: #fff;
-		font-size: 24rpx;
-	}
-}
+	.my_detail {
+		width: 100%;
+		height: 100vh;
+		background: #111;
+		display: flex;
+		flex-direction: column;
 
+		.eat-container {
+			width: 100%;
+			min-height: 0;
+			flex: 1;
+			padding: 20rpx;
+			overflow-y: auto;
+			
+			.video-box {
+				width: 100%;
+				.video-title {
+					margin-top: 15rpx;
+					font-size: 28rpx;
+					line-height: 40rpx;
+					color: #ddd;
+				}
+				.video-label {
+					margin-top: 20rpx;
+					display: flex;
+					align-items: center;
+					&-info {
+						position: relative;
+						top: 1rpx;
+						font-size: 24rpx;
+						color: #999;
+						font-weight: 400;
+						
+					}
+					&-item {
+						display: flex;
+						align-items: center;
+						color: #DB7FDB;
+						font-size: 24rpx;
+						font-weight: 400;
+						margin-left: 10rpx;
+						gap: 8px;
+						// .text {
+						// 	margin-right: 8rpx;
+						// }
+					}
+				}
+			}
+		}
 
+		.d_title {
+			color: #fff;
+			font-size: 28rpx;
+		}
+
+		.d_time {
+			margin-top: 18rpx;
+			color: #aaa;
+			font-size: 23rpx;
+		}
+
+		.d_cont {
+			padding: 20rpx 0;
+			color: #fff;
+			font-size: 24rpx;
+		}
+	}
 </style>
---
+++ 66video/pages/eat/components/list.vue
@@ -0,0 +1,78 @@
+<template>
+	<view class="list">
+		<view class="list-item" v-for="(item, index) in list" :id="'list'+item.id" :key="index" @click="toLink('/pages/detail/article', item.id)">
+			<image class="flag" src="/static/images/index/hot-icon.png" mode=""></image>
+			<image class="poster" :src="item.mobile_image" mode=""></image>
+			<view class="mask">{{ item.title }}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			list: {
+				type: Array,
+				default () {
+					return []
+				}
+			}
+		},
+		methods: {
+			toLink(url, id) {
+				uni.navigateTo({
+					url: `${url}?id=${id}`
+				})
+			}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.list {
+		display: flex;
+		flex-direction: column;
+		gap: 28rpx;
+		padding: 0 32rpx 28rpx;
+		box-sizing: border-box;
+
+		.list-item {
+			position: relative;
+			width: 100%;
+			height: 260rpx;
+			border-radius: 20rpx;
+
+			.flag {
+				position: absolute;
+				top: 0;
+				left: 20rpx;
+				width: 48rpx;
+				height: 48rpx;
+				z-index: 1;
+			}
+
+			.poster {
+				width: 100%;
+				height: 100%;
+			}
+
+			.mask {
+				width: 100%;
+				height: 100%;
+				background-color: rgba(0, 0, 0, 0.4);
+				position: absolute;
+				top: 0;
+				left: 0;
+				padding: 40rpx 76rpx;
+				box-sizing: border-box;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				font-size: 28rpx;
+				color: #fff;
+				font-weight: 600;
+				line-height: 40rpx;
+			}
+		}
+	}
+</style>
